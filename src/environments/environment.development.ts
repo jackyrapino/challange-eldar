075bca6499363c404fe9fe83e08818bd1887abd0
Jacqueline Maquez <jmarquez@valkiria>
2024-08-09 13:37:26 -0300

    feat(firebase):integro firebase al proyecto

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,11 @@
+export const environment = {
+    firebaseConfig:{
+        projectId: 'challengeeldar-47612',
+        appId: '1:575764580334:web:93dcba455e334b8da930ff',
+        storageBucket: 'challengeeldar-47612.appspot.com',
+        apiKey: 'AIzaSyACVfeH3j93wwnuBvgMuRKAIEOMqb_dAok',
+        authDomain: 'challengeeldar-47612.firebaseapp.com',
+        messagingSenderId: '575764580334',
+        measurementId: 'G-N17TD3ZC6P',
+      }
+};
